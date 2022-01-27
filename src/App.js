953d3ddb2b47a2999f4ff0a54334769aee4e53fe
Lee Simmons <44329129+Roman160@users.noreply.github.com>
2022-01-26 19:47:07 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useLayoutEffect } from 'react';
+import Dropdown from './components/Dropdown';
+import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import Home from './pages';
+import Gallery from './pages/Gallery';
+import SignIn from './pages/SignIn';
+import Order from './pages/Order';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme();
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+
+  console.log(location.pathname);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useLayoutEffect(
+    () => {
+      window.scrollTo(0, 0);
+    },
+    [location.pathname]
+  );
+
+
+
+  return (
+    <>
+     <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/gallery' component={Gallery} />
+        <Route path='/signin' component={SignIn} />
+        <Route path='/order' component={Order} />
+      </Switch>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
